aae6de29854e9e49d91393b52e750786b8f3eb6b
Tamer Durgun <tmr.durgun@gmail.com>
2019-08-19 22:55:00 +0300

    user model & seeder created

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,6 +14,7 @@
 $factory->define(App\User::class, function ($faker) {
     return [
         'name' => $faker->name,
+        'surname' => $faker->lastName,
         'email' => $faker->email,
         'password' => str_random(10),
         'remember_token' => str_random(10),
