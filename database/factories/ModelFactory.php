53c885bb94ae533a3ecc44a848e04bf0d38912b1
Tamer Durgun <tmr.durgun@gmail.com>
2019-08-19 18:39:47 +0300

    database setup & games model & games seeder created

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,9 @@ $factory->define(App\User::class, function ($faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Games::class, function ($faker) {
+    return [
+        'title' => $faker->unique()->colorName
+    ];
+});
