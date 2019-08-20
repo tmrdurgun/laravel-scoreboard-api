7b09e4c180f2bc0a438b96a57b55a475353480fc
Tamer Durgun <tmr.durgun@gmail.com>
2019-08-20 18:31:22 +0300

    game resource created

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -9,23 +9,22 @@ use App\Http\Controllers\Controller;
 
 use App\Game;
 use App\User;
+use App\Http\Resources\GameResource;
+use App\Http\Resources\GamesResource;
+
 
 class GameController extends Controller
 {
+
+    public function show(Game $game)
+    {
+        GameResource::withoutWrapping();
+        
+        return new GameResource($game);
+    }
+
     public function get_games()
     {
-        $gameModel = new Game();
-        $games = $gameModel->get();
-
-        $scores = [];
-        $users = [];
-
-        foreach($games as $game) {
-            $scores[] = $game->score;
-
-            $users[] = User::with('score')->get();
-        }
-
-        return $users;
+      $games = new GamesResource(Game::class);
     }
 }
